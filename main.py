ba63c444d973f13c55a89e7ca7ae53f8ac9eaa55
Hagoris <hsetwaiyantun2010@gmail.com>
2026-04-10 21:25:03 +0630

    Update main.py

--- main.py
+++ main.py
@@ -107,14 +107,13 @@ items_database = {
     "riess": 27
 }
 
-@bot.message_handler(commands=['value'])
-@bot.message_handler(commands=['val'])
+@bot.message_handler(commands=['value', 'val'])
 def copy_value(message):
     try:
         input_parts = message.text.split(maxsplit=1)
 
         if len(input_parts) < 2:
-            bot.send_message(message.chat.id, "💡 Usage: `/value [item name]`\n\n `/val [item name]`", parse_mode='Markdown')
+            bot.send_message(message.chat.id, "💡 Usage:\n`/value [item name]`\n`/val [item name]`", parse_mode='Markdown')
             return
 
         item_name = input_parts[1].lower().strip()
