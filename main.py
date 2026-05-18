68d59acbeb8a967b894b33a3a57e972555e2736a
Hagoris <hsetwaiyantun2010@gmail.com>
2026-05-18 21:29:35 +0630

    Update main.py

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ def send_welcome(message):
     intro_msg = (
         "👋 Welcome to Hikari Value Bot!\n\n"
         "This bot is designed for Attack on Titan: Revolution (AOT:R)  players to easily check "
-        "item values and explore recommended character builds.\n\n"
+        "item values and explore recommended Families builds.\n\n"
         "💡 To see how to use the bot, please click the command below:\n"
         "👉 /help"
     )
@@ -37,14 +37,16 @@ def send_welcome(message):
 def help_command(message):
     help_text = (
         "How to use Hikari Value Bot?\n\n"
-        "🔹 Check Item Values:\n"
+        "Check Item Values:\n"
         "Type: `/value [item name]` or `/val [item name]`\n"
         "_(Example: `/value fritz` )_\n\n"
-        "🔹 Check Build Guides:\n"
+        "Check Build Guides:\n"
         "Type: `/build [family name] [build name]`\n"
         "_(Example: `/build leonhart female` )_\n\n"
-        "🔹 Give Suggestions:\n"
+        "Give Suggestions:\n"
         "Type: `/suggestions` and then send your feedback message.\n\n"
+        "Check Item List:\n"
+        "Type: `/list`.\n\n"
         "📌 Note: Make sure to type the item names correctly."
     )
     bot.send_message(message.chat.id, help_text, parse_mode='Markdown')
