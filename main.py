294ec08a1327946d81b811f197114700e2d99624
Hagoris <hsetwaiyantun2010@gmail.com>
2026-05-18 22:06:40 +0630

    Update main.py

--- main.py
+++ main.py
@@ -38,16 +38,16 @@ def help_command(message):
     help_text = (
         "**How to use Hikari Value Bot?**\n\n"
         "**Check Item Values:**\n"
-        "**Type: `/value [item name]` or `/val [item name]`\n**"
-        "**_(Example: `/value fritz` )_**\n\n"
+        "**Type:** `/value [item name]` **or** `/val [item name]`\n"
+        "**_(Example:** `/value fritz` **)_**\n\n"
         "**Check Build Guides:**\n"
-        "**Type: `/build [family name] [build name]`**\n"
-        "**_(Example: `/build leonhart female` )_**\n\n"
+        "**Type:** `/build [family name] [build name]`\n"
+        "**_(Example:** `/build leonhart female` **)_**\n\n"
         "**Give Suggestions:**\n"
-        "**Type: `/suggestions` and then send your feedback message.**\n\n"
-        "**Check Item List:**\n"
-        "**Type: `/list`.**\n\n"
-        "📌 **Note: Make sure to type the item names correctly.**"
+        "**Type:** `/suggestions` **and then send your feedback message.**\n\n"
+        "**Check Item & Build List:**\n"
+        "**Type:** `/list`\n\n"
+        "**📌 Note: Make sure to type the item names correctly.**"
     )
     bot.send_message(message.chat.id, help_text, parse_mode='Markdown')
 
